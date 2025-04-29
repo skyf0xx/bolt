b2960b20b3510a05886c38fd7945c07cb938bcd7
skyfoxx <skyfoxx@sacred.finance>
2025-04-29 12:45:07 +1000

    chore: cleanup

--- main.lua
+++ main.lua
@@ -1,37 +1,11 @@
 -- DEX Aggregator - Main Entry Point
-local Constants = require('dex.utils.constants')
 local Logger = require('dex.utils.logger')
 Logger = Logger.createLogger("Main")
 local DexHandlers = require('dex.dexhandlers')
-local Init = require('dex.init')
-local Schema = require('dex.db.schema')
 
 -- This will store our application components
 local components = {}
 
--- Initialize the system on startup
-local function initialize()
-  Logger.info("Starting DEX Aggregator v" .. Constants.VERSION)
-
-  -- Setup database
-  local db, dbErr = Schema.init(Constants.DB.FILENAME)
-  if not db then
-    Logger.error("Database initialization failed", { error = dbErr })
-    return false
-  end
-
-  -- Setup schema
-  local success, schemaErr = Schema.setupSchema(db)
-  if not success then
-    Logger.error("Schema setup failed", { error = schemaErr })
-    return false
-  end
-
-  -- Initialize all components
-  components = Init.setupComponents(db)
-
-  return true
-end
 
 -- Register all message handlers - now directly using the handler functions
 Handlers.add("Initialize",
@@ -86,13 +60,7 @@ Handlers.add("PollingCycle",
   Handlers.utils.hasMatchingTag("Action", "PollingCycle"),
   DexHandlers.handlePollingCycle)
 
--- Initialize once on process start
-local success = initialize()
-if success then
-  Logger.info("DEX Aggregator initialization complete and handlers registered")
-else
-  Logger.error("DEX Aggregator initialization failed")
-end
+
 
 -- Initialize handlers module with components
 DexHandlers.init(components)
