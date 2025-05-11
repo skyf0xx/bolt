f5003c47357e15dff4bb21c72dbab647aac2d25c
skyfoxx <skyfoxx@sacred.finance>
2025-05-11 14:47:34 +1000

    chore: cleanup

--- populate.lua
+++ populate.lua
@@ -3,18 +3,7 @@ local json = require('json')
 
 
 local BOLT_PROTOCOL = 'cMoONh83OXPJxJXtVv4WgNw91XRAbSg0yYRNw1RsO18' --'AzsqUB479zLabNlI9LbrSukKE6nGUtIYkpu3zGc2nz8'
-Handlers.add('populate-botega', Handlers.utils.hasMatchingTag('Action', 'PopulateBotega'), function(msg)
-  assert(msg.From == ao.id, "Only authorized processes can call this handler")
-  ao.send({
-    Target = BOLT_PROTOCOL,
-    Action = "CollectData",
-    Source = "botega",
-    SaveToDb = 'true',
-    RebuildGraph = 'false'
-  }).onReply(function(response)
-    print('Aggregator has been Populated with fresh reserves')
-  end)
-end)
+
 
 Handlers.add('populate', Handlers.utils.hasMatchingTag('Action', 'Populate'), function(msg)
   assert(msg.From == ao.id, "Only authorized processes can call this handler")
